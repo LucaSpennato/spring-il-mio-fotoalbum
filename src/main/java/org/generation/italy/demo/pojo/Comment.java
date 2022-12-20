ef151964145013e8e4bb671b9b89de3e47b886ac
Luca Spennato <luca.spennato@gmail.com>
2022-12-20 12:10:55 +0100

    add: eager fetch

--- src/main/java/org/generation/italy/demo/pojo/Comment.java
+++ src/main/java/org/generation/italy/demo/pojo/Comment.java
@@ -3,6 +3,7 @@ package org.generation.italy.demo.pojo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,7 +26,7 @@ public class Comment {
 	@Lob
 	private String comment;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(nullable = true)
 	private Photo photo;
 	
