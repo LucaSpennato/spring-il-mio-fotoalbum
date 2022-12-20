84440e538220129ec2d0f347808ad6875a9f1273
Luca Spennato <luca.spennato@gmail.com>
2022-12-20 12:10:45 +0100

    add @repo annotation

--- src/main/java/org/generation/italy/demo/repo/CategoryRepo.java
+++ src/main/java/org/generation/italy/demo/repo/CategoryRepo.java
@@ -2,7 +2,9 @@ package org.generation.italy.demo.repo;
 
 import org.generation.italy.demo.pojo.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepo extends JpaRepository<Category, Integer> {
 
 }
--- src/main/java/org/generation/italy/demo/repo/TagRepo.java
+++ src/main/java/org/generation/italy/demo/repo/TagRepo.java
@@ -2,7 +2,9 @@ package org.generation.italy.demo.repo;
 
 import org.generation.italy.demo.pojo.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TagRepo extends JpaRepository<Tag, Integer> {
 
 }
