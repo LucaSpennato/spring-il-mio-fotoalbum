08c75c1fb6368bfa50d7dd14e66f475c944e86e5
Luca Spennato <luca.spennato@gmail.com>
2022-12-21 17:12:27 +0100

    adding comments

--- vue-frontend/src/components/MainComponent.vue
+++ vue-frontend/src/components/MainComponent.vue
@@ -1,28 +1,12 @@
 <template>
 
 <div class="container">
-  <div class="row">
+  <div class="row my-5">
 
-    <div v-for="photo in photos" :key="photo.id">
-
-      <div class="text-danger">
-        {{ photo.id }} - {{ photo.title }}
-      </div>
-      <div v-for="tag in photo.tags" :key="tag.id" class="text-success">
-        {{ tag.id }} - {{ tag.name }}
-      </div>
-      <div v-for="cat in photo.categories" :key="cat.id" class="text-primary">
-        {{ cat.id }} - {{ cat.name }}
-      </div>
-      <div v-for="c in photo.comments" :key="c.id">
-        {{ c.id }} - {{ c.comment }}
-      </div>
-
-    </div>
+    <PostComponent v-for="photo in photos" :key="photo.id" 
+      :post="photo" @emitComment=" getCommentPost"/>
 
   </div>
-
-
 
 </div>
 
@@ -30,20 +14,51 @@
 
 <script>
 import axios from 'axios'
+import PostComponent from './PostComponent.vue';
 const apiUrl = 'http://localhost:8080/api/1/photo'
 
 export default {
   name: "MainComponent",
+  components:{
+    PostComponent,
+  },
+  props:{
+    needle:{
+      type: String,
+      required: false,
+    }
+  },
+  watch:{
+    needle(oldne, newn){
+      if(oldne != newn){
+        this.filterSearch()
+      }
+    }
+  },
   data(){
     return{
       photos: {},
     }
   },
   methods:{
+    getCommentPost(param){
+      console.log(param)
+    },
     getAllPhoto(){
       axios.get(apiUrl + "/all")
       .then(res=>{
         console.log(res.data)
+        this.photos = res.data
+      })
+      .catch(e => {console.error(e)})
+    },
+
+    filterSearch(){
+      console.log(this.needle)
+      axios.get(apiUrl + "/search",{params: {
+        query: this.needle
+      }})
+      .then(res=>{
         this.photos = res.data
       })
       .catch(e => {console.error(e)})
---
+++ vue-frontend/src/components/PostComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+
+    <div class="col-6 border border-rounded my-3 p-4 m-auto">
+        <img :src="post.url" :alt="post.title + ` 's photo` " class="img-fluid">
+        <div class="fs-2">
+            {{ post.title }}
+        </div>
+        <div class="my-2 border-top border-dark">
+            <span v-for="tag in post.tags" :key="tag.id" class="text-secondary">
+            #{{ tag.name }}
+        </span>
+        </div>
+        <div class="my-2 border-top border-dark">
+            <span v-for="cat in post.categories" :key="cat.id" class="text-secondary">
+                -{{ cat.name }}
+            </span>
+        </div>
+        <div>
+            {{ post.comments.length }} Comments
+        </div>
+        <div v-for="cm in post.comments" :key="cm.id">
+            {{ cm.comment }}
+        </div>
+        <div class="mt-3">
+            <input type="text" class="border rounded me-2 py-1" v-model.trim="comt"
+                placeholder="Write a comment..." >
+            <button class="btn btn-success" @click="emitComment(post.id)">Send</button>
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+name: "PostComponent",
+props:{
+    post:{
+        type: Object,
+        required : false,
+    },
+},
+data() {
+    return {
+        comt: ''
+    }
+},
+methods: {
+    emitComment(id){
+        this.$emit("emitComment",    {infos: {comment: this.comt, id: id }} )
+        this.comt = ''
+    }
+
+},
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'Bootstrap';
+
+</style>
