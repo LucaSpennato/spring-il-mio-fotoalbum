820984050abb44059aae10ce947003fef907ac1d
Luca Spennato <luca.spennato@gmail.com>
2022-12-21 15:05:23 +0100

    axios install, main component for testing

---
+++ vue-frontend/src/components/MainComponent.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <div>MAIN</div>
+
+</template>
+
+<script>
+import axios from 'axios'
+const apiUrl = 'http://localhost:8080/api/1/photo'
+
+export default {
+  name: "MainComponent",
+  data(){
+    return{
+      photos: {},
+    }
+  },
+  methods:{
+    getAllPhoto(){
+      axios.get(apiUrl + "/all")
+      .then(res=>{
+        console.log(res.data)
+      })
+    }
+  },
+  created(){
+    this.getAllPhoto();
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
